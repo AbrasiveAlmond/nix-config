fcee98252526367e4625877c5185a73c870c8030
AbrasiveAlmond <qpearson.nz@gmail.com>
2024-12-02 12:45:50 +1300

    a

--- hosts/homelab/configuration.nix
+++ hosts/homelab/configuration.nix
@@ -129,16 +129,16 @@
       allowed-users = ["@root"];
       experimental-features = "nix-command flakes";
       # Opinionated: disable global registry
-      flake-registry = "";
+      # flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
+      # nix-path = config.nix.nixPath;
     };
     # Opinionated: disable channels
     channel.enable = false;
 
     # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    # registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   system.stateVersion = "24.11";
