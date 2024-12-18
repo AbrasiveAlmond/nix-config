35ab15a26bed05023a8a19fe4907bd7fbe9721ca
AbrasiveAlmond <qpearson.nz@gmail.com>
2024-12-18 21:19:13 +1300

    final sidebery tweaks for now + break down of homelab configuration

--- hosts/homelab/configuration.nix
+++ hosts/homelab/configuration.nix
@@ -15,7 +15,8 @@
   imports = [ # Include the results of the hardware scan.
     ./locale.nix
     ./hardware-configuration.nix
-    ./services/immich.nix
+    ./server.nix
+    # ./services/immich.nix
 
     # modules from nixos-hardware repo:
     inputs.hardware.nixosModules.common-cpu-intel
@@ -23,12 +24,8 @@
     # inputs.hardware.nixosModules.common-gpu-nvidia
   ];
 
-  # security.sudo.execWheelOnly = true;
-  security.sudo.enable = false;
-  security.lockKernelModules = true;
-  # security.auditd.enable = true;
-
-  # services.tailscale.enable = true;
+  # This file only defines the base system, 
+  # by removing extra imports it should be a functional & bare system.
 
   networking = {
     hostName = "homelab";
@@ -40,24 +37,17 @@
         prefixLength = 24;
       }];
     };
-
-    firewall = {
-      enable = true;
-      pingLimit = "--limit 1/minute --limit-burst 5";
-      allowedTCPPorts = [ 22 ];
-      # More ports opened by caddy.nix
-    };
   };
 
   services.openssh = {
     enable = true;
     ports = [ 22 ];
-    openFirewall = false; # port opened from firewall declaration
+    openFirewall = true;
 
     settings = {
       # TODO: Get rid of homelab user all together??
       AllowUsers = [ "homelab" ];
-      PermitRootLogin =  "no";
+      PermitRootLogin =  "no"; # can get root via homelab "bash> su" 
       PasswordAuthentication = false;
     };
   };
@@ -71,38 +61,25 @@
     ];
 
     openssh.authorizedKeys.keys = [
-#       "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-# QyNTUxOQAAACBx4sL1n+q7fybEsq//0DvBPui/Pr+asFsRxjtpI2IdpAAAAJhT4q65U+Ku
-# uQAAAAtzc2gtZWQyNTUxOQAAACBx4sL1n+q7fybEsq//0DvBPui/Pr+asFsRxjtpI2IdpA
-# AAAEBDAjt/UiohegHy7F4ZajaB4ne3ORzxWRWv+k8EYog6KHHiwvWf6rt/JsSyr//QO8E+
-# 6L8+v5qwWxHGO2kjYh2kAAAAFXF1aW5uaWVib2lAbWluaWZyaWRnZQ=="
-
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHHiwvWf6rt/JsSyr//QO8E+6L8+v5qwWxHGO2kjYh2k quinnieboi@minifridge"
     ];
-    # "SHA256:RIMu6Jf3HaTfTaOsjiTrB8ejunl9GVuPLmyIFFC50NA quinnieboi@minifridge"
   };
 
-
-  services.fail2ban = {
-    enable = true;
-    # Ban IP after 2 failures
-    maxretry = 2;
-    # ignoreIP = [
-    #   # Whitelist some subnets
-    #   "192.168.0.0/16"
-    # ];
-    bantime = "1min"; # Ban IPs for one day on the first ban
-    bantime-increment = {
-      enable = true; # Enable increment of bantime after each violation
-      multipliers = "1 2 4 8 16 32 64";
-      overalljails = true; # Calculate the bantime based on all the violations
-    };
-  };  
+# No fail2ban cause no passwords allowed
+  # services.fail2ban = {
+  #   enable = true;
+  #   # Ban IP after 2 failures
+  #   maxretry = 2;
+  #   bantime = "1min"; # Ban IPs for one day on the first ban
+  #   bantime-increment = {
+  #     enable = true; # Enable increment of bantime after each violation
+  #     multipliers = "1 2 4 8 16 32 64";
+  #     overalljails = true; # Calculate the bantime based on all the violations
+  #   };
+  # };
 
   environment.systemPackages = with pkgs; [
     vim
-    # git
-    # noip
   ];
   
   # Bootloader configuration
@@ -126,19 +103,12 @@
     flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
   in {
     settings = {
-      allowed-users = ["@root"];
+    # TODO: Only allowed user is root
+      # allowed-users = ["@root"];
       experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
-      # flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      # nix-path = config.nix.nixPath;
     };
     # Opinionated: disable channels
     channel.enable = false;
-
-    # Opinionated: make flake registry and nix path match flake inputs
-    # registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   system.stateVersion = "24.11";
