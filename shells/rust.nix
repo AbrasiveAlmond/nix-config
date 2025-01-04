65d55257c8480e1c5532723b52685dca313f38f7
AbrasiveAlmond <qpearson.nz@gmail.com>
2025-01-05 07:08:19 +1300

    delete other branch remenants

--- shells/rust.nix
+++
@@ -1,8 +0,0 @@
-
-{nixpkgs, ...}: {
-  name = "simple";
-  builder = "${nixpkgs.legacyPackages."x86_64-linux".bash}/bin/bash";
-  args = [ "-c" "echo foo > $out" ];
-  src = ./.;
-  system = "x86_64-linux";
-}
