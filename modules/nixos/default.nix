f637d79ed5b78c7f2da5402182e922e03113b3c4
AbrasiveAlmond <qpearson.nz@gmail.com>
2025-01-08 20:14:31 +1300

    remove some old stuff

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,8 +1,5 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
 {
-  outputs = inputs : {
-    # List your module files here
-    rust = import ./rust.nix;
-  };
+
 }
