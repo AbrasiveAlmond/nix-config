c294c15a9cee2b213348f1736e366e989476f272
AbrasiveAlmond <qpearson.nz@gmail.com>
2025-02-13 10:25:45 +1300

    added immich.nix

---
+++ hosts/minifridge/immich.nix
@@ -0,0 +1,25 @@
+{pkgs,...}: {
+  services.immich  = {
+    enable = true;
+    package = pkgs.immich;
+    host = "100.99.19.37";
+    port = 2283;
+    mediaLocation = "/srv/immich";
+    openFirewall = false;
+
+    user = "immich";
+    group = "immich";
+  };
+
+  # If you are using Network Manager, you need to explicitly prevent it from managing container interfaces:
+  # networking.networkmanager.unmanaged = [ "interface-name:ve-*" ];
+
+  # for hardware accelerated video transcoding using VA-API
+  users.users.immich.extraGroups = [ "video" "render" ];
+
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+
+  users.users.immich = {
+      isNormalUser = false;
+  };
+}
