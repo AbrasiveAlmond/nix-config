972311d10973c6b020e2c82f7825517efb5ffdad
AbrasiveAlmond <qpearson.nz@gmail.com>
2026-02-11 09:44:05 +1300

    [fix] Disable VRR as it makes 2nd display unstable

--- common/nixos/gnome/gnome.nix
+++ common/nixos/gnome/gnome.nix
@@ -35,7 +35,7 @@ in
           "org/gnome/mutter" = {
             experimental-features = [
               "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
-              "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+              # "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
               "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
               "autoclose-xwayland" # automatically terminates Xwayland if all relevant X11 clients are gone
             ];
